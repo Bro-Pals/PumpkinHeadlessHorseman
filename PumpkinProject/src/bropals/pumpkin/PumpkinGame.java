d1f291d343303ab4c8c5f58c510bd795078cf437
RadioLemon <horsentp@gmail.com>
2015-03-01 08:21:57 -0600

    Made empty jmonkey project

---
+++ PumpkinProject/src/bropals/pumpkin/PumpkinGame.java
@@ -0,0 +1,36 @@
+package bropals.pumpkin;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class PumpkinGame extends SimpleApplication {
+
+    public static void main(String[] args) {
+        PumpkinGame app = new PumpkinGame();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        
+    }
+}
